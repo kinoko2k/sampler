be53a99421f426ec1443d8b89afdc70332cde499
Kinoko_2k <65443692+kinoko2k@users.noreply.github.com>
2021-12-16 17:50:22 +0900

    a

--- sounds.js
+++ sounds.js
@@ -6,4 +6,11 @@ function sound()
 		document.getElementById( id ).currentTime = 0;
 	}
 	document.getElementById( id ).play() ;
+
+	var id2 = 'sound-file1' ;
+	if( typeof( document.getElementById( id ).currentTime ) != 'undefined' )
+	{
+		document.getElementById( id ).currentTime = 0;
+	}
+	document.getElementById( id2 ).play() ;
 }
