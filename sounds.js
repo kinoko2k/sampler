a3fe9b79fbbd0395f72d8725c9acfcf5fb7abd39
Kinoko_2k <65443692+kinoko2k@users.noreply.github.com>
2021-12-16 17:24:00 +0900

    a

--- sounds.js
+++ sounds.js
@@ -7,18 +7,3 @@ function sound()
 	}
 	document.getElementById( id ).play() ;
 }
-(function (window, $) {
-	'use strict';
-  
-	$.fn.useSound = function (_event, _id) {
-	  var se = $(_id);
-	  this.on(_event, function(){
-		se[0].currentTime = 0;
-		se[0].play();
-	  });
-	  return this;
-	};
-  
-  })(this, this.jQuery);
-  
-  $('.btn a').useSound('mousedown touchstart', '#sound');
